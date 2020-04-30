846d91167b6460a0636e2283d8b81b2b3007a197
usayyed <ulfatsayyed@outlook.com>
2020-04-29 22:22:36 -0400

    Enabled cors headers

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const winston = require('./server/config/winston');
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const winston = require("./server/config/winston");
 
-let config = require('./server/config/config.json');
+let config = require("./server/config/config.json");
 
-config = config[process.env.NODE_ENV || 'development']
+config = config[process.env.NODE_ENV || "development"];
 
 // Set up the express app
 const app = express();
 // Add Cors support
 app.use(cors());
 
+app.use(function (req, res, next) {
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "Authorization,Content-Type, Content-Range, Content-Disposition, Content-Description,Origin, X-Requested-With, sessionId"
+  );
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 // Log requests to the console.
-app.use(logger('combined', { stream: winston.stream }));
+app.use(logger("combined", { stream: winston.stream }));
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Require our routes into the application.
-require('./server/routes')(app);
+require("./server/routes")(app);
 // Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
-}));
+app.get("*", (req, res) =>
+  res.status(200).send({
+    message: "Welcome to the beginning of nothingness.",
+  })
+);
 
 module.exports = app;
