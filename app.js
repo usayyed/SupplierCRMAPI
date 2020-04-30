80b50da2f91d2375ebdd9db7085ede7673900774
usayyed <ulfatsayyed@outlook.com>
2020-04-30 00:28:45 -0400

    save

--- app.js
+++ app.js
@@ -13,17 +13,9 @@ const app = express();
 // Add Cors support
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Authorization, Content-Type, Content-Range, Content-Disposition, Content-Description,Origin, X-Requested-With, sessionId"
-  );
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, Content-Type, Content-Range, Content-Disposition, Content-Description,Origin, X-Requested-With, sessionId"
-  );
-  res.header("Access-Control-Allow-Origin", "*");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
